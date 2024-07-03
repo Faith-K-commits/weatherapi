4a6dd066d8c905d877430618f868f1203fe0f8ff
Faith Kariuki <tcwamboi@gmail.com>
2024-07-03 13:50:07 +0300

    Remove env

--- base/settings.py
+++ base/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,8 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-IPSTACK_API_KEY = config('IPSTACK_API_KEY')
-WEATHER_API_KEY = config('WEATHER_API_KEY')
+
 
 
 # Application definition
